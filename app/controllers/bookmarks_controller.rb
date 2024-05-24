ec7eee04bea417d0f4ec36e646a463b52d12988f
Thomas Cullum <thomascullum05@gmail.com>
2024-05-24 16:13:06 +0200

    Add dotenv-rails

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,22 +1,20 @@
 class BookmarksController < ApplicationController
 
   def new
-     @list = List.find(params[:list_id])
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    # @list = List.find(params[:list_id])
     @bookmark.save
-    redirect_to list_path(@list)
-
+    redirect_to lists_path
   end
 
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
 end
